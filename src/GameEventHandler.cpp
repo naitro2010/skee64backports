86e16d75c7cf8f5a1fc7c7a47d09054f0f042faa
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2026-03-18 02:22:10 -0700

    remove Load3D

--- src/GameEventHandler.cpp
+++ src/GameEventHandler.cpp
@@ -252,12 +252,6 @@ namespace plugin {
                             }
                             auto &data = recalcs_in_progress[p.first];
                             if (auto actor = p.second.get()) {
-                                if (!actor->Is3DLoaded()) {
-                                    if (auto node=actor->Load3D(true)) {
-                                        actor->Set3D(node);
-                                    }
-                                    
-                                }
                                 if (actor->Is3DLoaded()) {
                                     if (auto actor_biped = actor->GetCurrentBiped()) {
                                         bool found_ube = false;
@@ -493,6 +487,9 @@ namespace plugin {
                 if (a_event && a_event->cell) {
                     a_event->cell->ForEachReference([](RE::TESObjectREFR *ref) {
                         if (auto actor = ref->As<RE::Actor>()) {
+                            if (!actor->Is3DLoaded()) {
+                                actor->Load3D(false);
+                            }
                             if (RE::PlayerCharacter::GetSingleton()) {
                                 AddActorToRecalculate(actor);
                             }
